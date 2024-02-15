a9bf5cdba533cbfe01bb0f7afad50dbdc8768ff0
csuraparaju <csuraparaju@wpi.edu>
2024-02-15 18:42:36 -0500

    package.json update

--- src/posts/parse.js
+++ src/posts/parse.js
@@ -5,12 +5,13 @@ const url = require('url');
 const winston = require('winston');
 const sanitize = require('sanitize-html');
 const _ = require('lodash');
+const katex = require('katex'); // Added for latex rendering in posts
 
 const meta = require('../meta');
 const plugins = require('../plugins');
 const translator = require('../translator');
 const utils = require('../utils');
-const katex = require('katex'); // Added for latex rendering in posts
+
 
 let sanitizeConfig = {
     allowedTags: sanitize.defaults.allowedTags.concat([
@@ -53,9 +54,9 @@ module.exports = function (Posts) {
        * and rendering LaTeX code into MathML using KaTeX.
        * @param {object} postData - the post object
        * @return {object} postData - the sanitized/rendered post object
-   */
-  /*
-   Type definition for the postData object:
+    */
+    /*
+    Type definition for the postData object:
        type PostObject = {
            pid: number;
            tid: number;
@@ -73,7 +74,7 @@ module.exports = function (Posts) {
            isMainPost: boolean;
            replies: number;
        };
-  */
+    */
    Posts.parsePost = async function (postData) {
         // Assert the type of input is correct, and that the content is a string
         console.assert(typeof postData === 'object', 'postData.pid is not an object');
@@ -97,19 +98,19 @@ module.exports = function (Posts) {
             * @return {object} postData - the post object with LaTeX code rendered into MathML
             * @throws {Error} - if the rendering fails
         */
-        const renderLatex = function(postData) {
+        const renderLatex = function (postData) {
             console.assert(typeof postData === 'object', 'postData.pid is not an object');
             console.assert(typeof postData.content === 'string', 'postData.content is not a string');
 
             const block = /\$\$([\s\S]*?)\$\$/g; // regex to match $$...$$
             const inline = /\$([\s\S]*?)\$/g; // regex to match $...$
 
-            const replaceBlock = function(match, p1, offset, string) {
+            const replaceBlock = function (match, p1, offset, string) {
                 // chose to only render using mathml, sacrificing compatibility
                 // with older browsers for better performance
                 return katex.renderToString(p1, {displayMode: true, output: 'mathml'});
             };
-            const replaceInline = function(match, p1, offset, string) {
+            const replaceInline = function (match, p1, offset, string) {
                 return katex.renderToString(p1, {displayMode: false, output: 'mathml'});
             }
             try {
