62289d7e9b7ab2313627eafbaa6ac9587776beb5
Vy <112414752+vynttran@users.noreply.github.com>
2024-03-29 16:54:49 -0400

    Update index.js

--- src/translate/index.js
+++ src/translate/index.js
@@ -1,9 +1,32 @@
-var request = require('request')
+// var request = require('request')
+
+// const translatorApi = module.exports;
+
+// translatorApi.translate = async function (postData) {
+//     const response = await fetch(process.env.TRANSLATOR_API+'/?content='+postData.content);
+//     const data = await response.json();
+//     return [data["is_english"], data["translated_content"]]
+// }
+
+const fetch = require('node-fetch');
 
 const translatorApi = module.exports;
 
 translatorApi.translate = async function (postData) {
-    const response = await fetch(process.env.TRANSLATOR_API+'/?content='+postData.content);
+    // Ensure the TRANSLATOR_API environment variable is set
+    if (!process.env.TRANSLATOR_API) {
+        throw new Error('TRANSLATOR_API environment variable is not set.');
+    }
+    
+    // Construct the URL
+    const apiUrl = new URL('/?content=' + encodeURIComponent(postData.content), process.env.TRANSLATOR_API);
+    
+    // Make the fetch request
+    const response = await fetch(apiUrl.toString());
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
     const data = await response.json();
-    return [data["is_english"], data["translated_content"]]
-}
+
+    return [data["is_english"], data["translated_content"]];
+};
